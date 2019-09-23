d298e223f1b26e513678f08cc0eba5522bc476c5
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-23 01:08:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,10 +4,17 @@ def "mario" do
   expect{Mario}.to output ("It's-a me, Mario?\n").to_stdout
 end
 
-def toadstool
+def toadstool 
   puts status
 end
 
-def "link"
+def "link" do
   puts phrase = "It's Dangerous To Go Alone! Take This."
 end
+
+Def "any_phrase" do
+  it "takes in an argument and puts out the catch phrase" do
+    phrase = "Do A Barell Roll!"
+    expect {any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+  end
+  
