90b4096aaa122ed218227602d9fcedcfcf808246
Lakisha Aldridge <lakishaaldridge@gmail.com>
2019-09-23 00:48:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,6 @@
 def "mario" do
   it "puts out 'It's-a me, Mario!'" do
-  puts phrase = 
+  puts phrase = "It's-a me, Mario!"
+  expect{Mario}.to output ("It's-a me, Mario?\n").to_stdout
 end
 
-def toadstool
-  puts status
-end
